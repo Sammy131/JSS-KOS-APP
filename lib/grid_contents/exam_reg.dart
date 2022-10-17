279dee49e21e227dcbfefac4434af9a4531cf687
SAMMY <114558761+Sammy131@users.noreply.github.com>
2022-10-17 16:42:31 +0530

    Update exam_reg.dart

--- lib/grid_contents/exam_reg.dart
+++ lib/grid_contents/exam_reg.dart
@@ -7,7 +7,7 @@ class ExamReg extends StatelessWidget {
           backgroundColor: Color.fromARGB(255, 214, 84, 67),
           centerTitle: true,
           title: Text('REGISTER FOR YOUR UPCOMING EXAM!!'),
-        ),
-        backgroundColor: Color.fromARGB(255, 97, 242, 223),
+        ),https://github.com/Sammy131/JSSKOS_APP/blob/main/lib/grid_contents/exam_reg.dart
+        backgroundColor: Color.fromARGB(248, 8, 38, 34),
       );
 }
