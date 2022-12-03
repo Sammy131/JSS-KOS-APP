964b4f3e746909cf435ee27a5e0f31f37e0601fc
SAMMY <114558761+Sammy131@users.noreply.github.com>
2022-12-03 15:33:54 +0530

    Add files via upload

---
+++ lib/screens/about.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+//import 'package:first_app/screens/home.dart';
+
+class about extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            backgroundColor: Color.fromARGB(255, 214, 84, 67),
+            centerTitle: true,
+            title: Text('ABOUT JSSKOS!!')),
+        //backgroundColor: Color.fromARGB(255, 97, 242, 223),
+        backgroundColor: Color.fromARGB(248, 8, 38, 34),
+        body: Center(
+            child: Text(
+          'info...',
+          style: TextStyle(fontSize: 20, color: Colors.white),
+        )));
+    //floatingActionButton: FloatingActionButton(onPressed: () async {
+    //await _auth.signOut()
+  }
+}
